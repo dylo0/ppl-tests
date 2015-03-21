28b3e5eb877305bb4ee5da70e2e0c6d24cab3ceb
Tomasz Dyl <tomekdyl@gmail.com>
2015-03-21 12:08:06 +0100

    Fixed refreshing summary view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,129 +1,4 @@
 angular.module('pplTester.controllers', [])
-
-
-    .controller('LearnCtrl', function ($scope, Questions) {
-        $scope.question = Questions.random();
-
-        $scope.nextQuestion = function () {
-            $scope.question = Questions.random();
-        };
-
-        $scope.$on("$ionicView.enter", function () {
-            if (Questions.changed()) {
-                $scope.nextQuestion();
-            }
-        });
-    })
-
-
-    .controller('QuizSummaryCtrl', function ($scope, Questions, Quizzes) {
-        this.quiz = Quizzes.getCurrentQuiz();
-        Quizzes.summaryShown();
-    })
-
-
-    .controller('QuestionsCtrl', function ($scope, Questions) {
-        var quiz = this;
-        quiz.test = Questions.randomQuestion();
-        quiz.choice = {};
-
-        quiz.checkAnswer = function (test, selectedAns) {
-            quiz.answered = true;
-            quiz.correct = this.test.correct === selectedAns;
-
-            Questions.updateAnswered(test, quiz.correct);
-        };
-
-        quiz.nextTest = function () {
-            quiz.answered = false;
-            quiz.choice = {};
-            quiz.test = Questions.randomQuestion();
-        };
-
-        $scope.$on("$ionicView.enter", function () {
-            if (Questions.changed()) {
-                quiz.test = Questions.randomQuestion();
-            }
-        });
-    })
-
-
-    .controller('QuizCtrl', function ($scope, Questions, Quizzes, $state, $stateParams, $ionicScrollDelegate) {
-        var quiz = this;
-
-        quiz.topics = Quizzes.getAllQuizzes();
-
-        quiz.scrollBottom = function () {
-            $ionicScrollDelegate.scrollBottom(true);
-        };
-
-        quiz.startQuiz = function (quiz) {
-            Quizzes.startNewQuiz(quiz);
-
-            $state.go('tab.quiz-question', {id: 0});
-        };
-    })
-
-    .controller('QuizQuestionsCtrl', function ($scope, Quizzes, $state, $stateParams, $translate, $ionicPopup, Questions) {
-        var quiz = this;
-        $scope.$on("$ionicView.enter", function () {
-            quiz.current = Quizzes.getCurrentQuiz();
-            if (quiz.current.ended && !quiz.current.scoreDisplayed) {
-                $state.go('tab.quiz-summary');
-            }
-        });
-
-        quiz.test = '';
-        quiz.choice = {};
-        quiz.current = Quizzes.getCurrentQuiz();
-        quiz.question = Quizzes.getQuizQuestion($stateParams.id);
-        quiz.currentIdx = parseInt($stateParams.id) + 1;
-
-        var submitAnswer = function (test, choice) {
-            //var correct = quiz.correct === choice;
-            Questions.updateAnswered(test, choice);
-            Quizzes.checkAnswer($stateParams.id, choice);
-
-            if (parseInt($stateParams.id) === quiz.current.count - 1) {
-                Quizzes.endQuiz();
-                $state.go('tab.quiz-summary');
-
-            } else {
-                $state.go('.', {
-                    id: parseInt($stateParams.id) + 1
-                });
-            }
-        };
-
-        var confirmEmpty = function (test, choice) {
-            $translate(['confirm', 'want_to_submit_empty', 'yes', 'no_return'])
-                .then(function (translations) {
-                    var confirmPopup = $ionicPopup.confirm({
-                        title: translations['confirm'],
-                        template: translations['want_to_submit_empty'],
-                        okText: translations['yes'],
-                        cancelText: translations['no_return'],
-                        okType: 'button-assertive'
-                    });
-
-                    confirmPopup.then(function (res) {
-                        if (res) {
-                            submitAnswer(test, choice);
-                        }
-                    });
-                });
-        };
-
-        quiz.checkAnswer = function (test, choice) {
-            if (angular.equals(choice, {})) {
-                confirmEmpty();
-            } else {
-                submitAnswer(test, choice);
-            }
-        };
-    })
-
-
     .controller('DashCtrl', function ($scope, Questions, $ionicPopup, Quizzes, $translate, $state, $ionicModal) {
         $scope.topics = Questions.getAllTopics();
         $scope.prepareQuestions = Questions.prepareQuesitons;
@@ -189,4 +64,133 @@ angular.module('pplTester.controllers', [])
         $scope.$on('$destroy', function () {
             $scope.modal.remove();
         });
+    })
+
+
+    .controller('LearnCtrl', function ($scope, Questions) {
+        $scope.question = Questions.random();
+
+        $scope.nextQuestion = function () {
+            $scope.question = Questions.random();
+        };
+
+        $scope.$on("$ionicView.enter", function () {
+            if (Questions.changed()) {
+                $scope.nextQuestion();
+            }
+        });
+    })
+
+
+    .controller('QuestionsCtrl', function ($scope, Questions) {
+        var questinCtrl = this;
+        questinCtrl.test = Questions.randomQuestion();
+        questinCtrl.choice = {};
+
+        questinCtrl.checkAnswer = function (test, selectedAns) {
+            questinCtrl.answered = true;
+            questinCtrl.correct = this.test.correct === selectedAns;
+
+            Questions.updateAnswered(test, questinCtrl.correct);
+        };
+
+        questinCtrl.nextTest = function () {
+            questinCtrl.answered = false;
+            questinCtrl.choice = {};
+            questinCtrl.test = Questions.randomQuestion();
+        };
+
+        $scope.$on("$ionicView.enter", function () {
+            if (Questions.changed()) {
+                questinCtrl.test = Questions.randomQuestion();
+            }
+        });
+    })
+
+
+    .controller('QuizCtrl', function ($scope, Questions, Quizzes, $state, $stateParams, $ionicScrollDelegate) {
+        var quiz = this;
+
+        quiz.topics = Quizzes.getAllQuizzes();
+
+        quiz.scrollBottom = function () {
+            $ionicScrollDelegate.scrollBottom(true);
+        };
+
+        quiz.startQuiz = function (quiz) {
+            Quizzes.startNewQuiz(quiz);
+
+            $state.go('tab.quiz-question', {id: 0});
+        };
+    })
+
+    .controller('QuizQuestionsCtrl', function ($scope, Quizzes, $state, $stateParams, $translate, $ionicPopup, Questions) {
+        var quiz = this;
+
+        quiz.test = '';
+        quiz.choice = {};
+        quiz.current = Quizzes.getCurrentQuiz();
+        quiz.question = Quizzes.getQuizQuestion($stateParams.id);
+        quiz.currentIdx = parseInt($stateParams.id) + 1;
+
+        $scope.$on("$ionicView.enter", function () {
+            quiz.current = Quizzes.getCurrentQuiz();
+            console.log('asd');
+            if (quiz.current.ended && !quiz.current.scoreDisplayed) {
+                console.log('ended');
+                $state.go('tab.quiz-summary');
+            }
+        });
+
+        var submitAnswer = function (test, choice) {
+            //var correct = quiz.correct === choice;
+            Questions.updateAnswered(test, choice);
+            Quizzes.checkAnswer($stateParams.id, choice);
+
+            if (parseInt($stateParams.id) === quiz.current.count - 1) {
+                Quizzes.endQuiz();
+                $state.go('tab.quiz-summary');
+
+            } else {
+                $state.go('.', {
+                    id: parseInt($stateParams.id) + 1
+                });
+            }
+        };
+
+        var confirmEmpty = function (test, choice) {
+            $translate(['confirm', 'want_to_submit_empty', 'yes', 'no_return'])
+                .then(function (translations) {
+                    var confirmPopup = $ionicPopup.confirm({
+                        title: translations['confirm'],
+                        template: translations['want_to_submit_empty'],
+                        okText: translations['yes'],
+                        cancelText: translations['no_return'],
+                        okType: 'button-assertive'
+                    });
+
+                    confirmPopup.then(function (res) {
+                        if (res) {
+                            submitAnswer(test, choice);
+                        }
+                    });
+                });
+        };
+
+        quiz.checkAnswer = function (test, choice) {
+            if (angular.equals(choice, {})) {
+                confirmEmpty();
+            } else {
+                submitAnswer(test, choice);
+            }
+        };
+    })
+
+    .controller('QuizSummaryCtrl', function ($scope, Questions, Quizzes) {
+        this.quiz = Quizzes.getCurrentQuiz();
+
+        $scope.$on("$ionicView.enter", function () {
+            Quizzes.summaryShown();
+        });
     });
+
